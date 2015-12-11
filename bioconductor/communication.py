c16ff01727561133024aad46cf37e94ef69f13a3
b-long <b-long@users.noreply.github.com>
2015-12-11 11:13:05 -0500

    Turn down logging on module search path

--- bioconductor/communication.py
+++ bioconductor/communication.py
@@ -8,7 +8,7 @@ logging.basicConfig(format='%(levelname)s: %(asctime)s %(filename)s - %(message)
                     datefmt='%m/%d/%Y %I:%M:%S %p',
                     level=logging.INFO)
                     
-logging.debug("The module search path is: \n%s", sys.path)
+logging.trace("The module search path is: \n%s", sys.path)
 
 from stompy import Stomp as oldStompConstructor
 import stomp
