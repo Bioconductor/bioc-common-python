5fd4810e8dbd1927fb4751346a2dbc00871a4ea3
b-long <b-long@users.noreply.github.com>
2015-12-23 13:59:13 -0500

    Initial unit test framework

--- tests/test_communication.py
+++ tests/test_communication.py
@@ -1,9 +1,18 @@
-from bioconductor.communication import getOldStompConnection
-from testify import *
-from bioconductor.config import BROKER
+# Platform dependencies
 import sys
+import logging
 
-class AdditionTestCase(TestCase):
+# Logging framework
+from testify import *
+
+# Stomp dependencies
+from stomp.listener import PrintingListener
+
+# Bioconductor dependencies
+from bioconductor.communication import getNewStompConnection
+from bioconductor.config import BROKER
+
+class CommunicationTestCase(TestCase):
 
     @class_setup
     def init_the_variable(self):
@@ -16,21 +25,20 @@ class AdditionTestCase(TestCase):
     def testHostLineEnding(self):
         assert_equal(BROKER['host'].endswith('\n'), False)
 
-    def testGetOldConn(self):
-        conn = getOldStompConnection()
-        assert_equal(type(conn) is str, False)
+    def testConn(self):
+        conn = getNewStompConnection('', PrintingListener())
+        assert_not_equal(conn, None)
 
     def testSubscribeToConn(self):
         try:
-            stomp = getOldStompConnection()
+            stomp = getNewStompConnection('', PrintingListener())
             print "Client is: '%s'" % stomp
         except:
             logging.error("Cannot connect to Stomp")
             raise
 
-        retVal = stomp.subscribe({'destination': "/topic/builderevents", 'ack': 'client'})
-        print "Subscribe retval is: '%s'" % retVal
-        sys.stdout.flush()
+        stomp.subscribe(destination="/topic/builderevents", id="StompUnitTest",
+                    ack='auto')
         assert_equal(type("conn") is str, True)
 
     def testPortLineEnding(self):
