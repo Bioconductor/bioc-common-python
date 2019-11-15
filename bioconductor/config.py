ab6cbade730cc611b4edddcc4446324df6d05a94
Hervé Pagès <hpages@fredhutch.org>
2019-11-15 09:33:09 -0800

    Convert code from Python 2 to Python 3 using 2to3

--- bioconductor/config.py
+++ bioconductor/config.py
@@ -4,7 +4,7 @@ import os
 import os.path
 import platform
 import logging
-import ConfigParser
+import configparser
 
 # log.basicConfig(format='%(levelname)s: %(asctime)s %(filename)s - %(message)s',
 #                     datefmt='%m/%d/%Y %I:%M:%S %p',
@@ -29,7 +29,7 @@ if not readFile(GLOBAL_PROPERTIES_FILE):
     raise Exception(errMsg)
 
 # Parse and read the file
-globalConfigParser = ConfigParser.RawConfigParser()
+globalConfigParser = configparser.RawConfigParser()
 globalConfigParser.read(GLOBAL_PROPERTIES_FILE)
 
 environment = globalConfigParser.get('Environment', 'environment')
@@ -47,10 +47,10 @@ if not readFile(ENVIRONMENT_PROPERTIES_FILE):
 log.info("Environment is set to: '{env}'.".format(env = environment))
 
 # Parse and read the environment specific configuration
-envConfig = ConfigParser.RawConfigParser()
+envConfig = configparser.RawConfigParser()
 envConfig.read(ENVIRONMENT_PROPERTIES_FILE)
 
-sensitiveConfigParser = ConfigParser.RawConfigParser()
+sensitiveConfigParser = configparser.RawConfigParser()
 sensitiveConfigParser.read(SENSITIVE_PROPERTIES_FILE)
 
 # FIXME: Rather than attempting to read the same properties in any environment,
